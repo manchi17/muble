1b1ccf3ce93b94394a1d5ca1537fd2e841652c8b
manchi17 <manseed.rt7@gmail.com>
2021-03-09 16:03:17 +0530

    Added image

--- src/screens/addItemScreen.js
+++ src/screens/addItemScreen.js
@@ -1,5 +1,6 @@
 import {
   StyleSheet,
+  Image,
   Text,
   View,
   SafeAreaView,
@@ -11,7 +12,6 @@ import { COLORS } from "../styles/colors";
 import { common } from "../styles/common";
 import { getItem } from "../ducks/item";
 import { Entypo } from "@expo/vector-icons";
-
 
 class AddItemScreen extends Component {
   constructor(props) {
@@ -25,9 +25,35 @@ class AddItemScreen extends Component {
     return (
       <SafeAreaView style={common.container}>
         <View style={styles.headerConatiner}>
-        <Entypo name={"menu"} size={20} color={COLORS.black} style={styles.menuIcon} />
-        <View style={styles.subHeader}><Text style={styles.subHeaderText}>CONSTRUCTOR</Text></View>
-        <Entypo name={"shopping-cart"} size={20} color={COLORS.black} style={styles.cartIcon}/>
+          <Entypo
+            name={"menu"}
+            size={20}
+            color={COLORS.black}
+            style={styles.menuIcon}
+          />
+          <View style={styles.subHeader}>
+            <Text style={styles.subHeaderText}>CONSTRUCTOR</Text>
+          </View>
+          <Entypo
+            name={"shopping-cart"}
+            size={20}
+            color={COLORS.black}
+            style={styles.cartIcon}
+          />
+        </View>
+        <View style={styles.mainContainer}>
+          {this.props.items && (
+            <View>
+              <Image
+                style={styles.image}
+                resizeMode={"cover"}
+                source={{
+                  uri: this.props.items.brands.image,
+                }}
+              ></Image>
+              <Text>{this.props.items.brands.name}</Text>
+            </View>
+          )}
         </View>
         <TouchableOpacity style={styles.buttonContainer}>
           <Text style={styles.buttonText}>ADD TO CART</Text>
@@ -42,13 +68,15 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
-
   },
-  headerConatiner:{
-     flexDirection:'row'
+  headerConatiner: {
+    flexDirection: "row",
+  },
+  mainContainer: {
+    paddingLeft: 20,
   },
   buttonContainer: {
-    position:"absolute",
+    position: "absolute",
     backgroundColor: COLORS.blue,
     width: "100%",
     height: 30,
@@ -56,37 +84,41 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     bottom: 0,
   },
+  image: {
+    width: 80,
+    height: 30,
+  },
   buttonText: {
     color: COLORS.white,
     fontSize: 16,
     fontWeight: "bold",
   },
-  subHeader:{
-    flex:1,
-    height:50,
-    backgroundColor:COLORS.yellow,
-    alignItems:"center",
-    justifyContent:"center",
-    borderWidth:1,
-    borderColor:COLORS.yellow,
-    borderBottomLeftRadius:10,
-    borderBottomRightRadius:10
+  subHeader: {
+    flex: 1,
+    height: 50,
+    backgroundColor: COLORS.yellow,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: COLORS.yellow,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
   },
   subHeaderText: {
     color: COLORS.black,
     fontSize: 20,
     fontWeight: "bold",
   },
-  cartIcon:{
-    marginRight:10,
-    marginLeft:20,
-    alignSelf:"center"
+  cartIcon: {
+    marginRight: 10,
+    marginLeft: 20,
+    alignSelf: "center",
   },
-  menuIcon:{
-    marginRight:20,
-    marginLeft:10,
-    alignSelf:"center"
-  }
+  menuIcon: {
+    marginRight: 20,
+    marginLeft: 10,
+    alignSelf: "center",
+  },
 });
 
 const mapStateToProps = (state) => ({
